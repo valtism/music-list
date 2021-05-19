70d05d567d7174b192adee17c1d20a44a5d5ca94
Dan Wood <daniel4wood@icloud.com>
2021-05-19 22:45:16 +1000

    Config setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import svgr from "vite-plugin-svgr";
+import eslint from "@rollup/plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    svgr(),
+    eslint({ include: ["./src/**/*.vue", "./src/**/*.js"] }),
+  ],
 });
