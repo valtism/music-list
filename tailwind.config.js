2d1e496d1d8086905253c937fa0be558af27063e
Dan Wood <daniel4wood@icloud.com>
2021-06-26 05:03:42 +1000

    Use vite-plugin-eslint
    Better performance linting than the rollup plugin
    
    Extend config to deal with "module" not being defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "vite-plugin-svgr";
-import eslint from "@rollup/plugin-eslint";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
     svgr(),
-    eslint({ include: ["./src/**/*.vue", "./src/**/*.js"] }),
+    eslintPlugin(),
   ],
   base: "/music-list/",
   build: {
