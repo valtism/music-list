3f1c36259aaf21133a2df6225ec50d4b27cd0e76
Dan Wood <daniel4wood@icloud.com>
2021-07-04 23:16:48 +1000

    Fix download
    Using an anchor link with "download" and href being set on click did
    not wait for the href to be changed to start the download. Go back to
    generating an anchor and simulating a click.

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -5,18 +5,21 @@ import { gridAlbums } from "../state/albumState";
 import { getImageDataUrl } from "../util/getImageDataUrl";
 
 export default function DownloadButton() {
-  const ref = useRef<HTMLAnchorElement>(null);
+  const ref = useRef<HTMLButtonElement>(null);
   const albums = useAtomValue(gridAlbums);
   const urls = albums.map((album) => album?.images[0].url);
 
   return (
-    <a
-      download
+    <button
       ref={ref}
       onClick={async () => {
         const dataUrl = await getImageDataUrl(urls, 3, 3);
         if (!ref.current || !dataUrl) return;
-        ref.current.href = dataUrl;
+        const a = document.createElement("a");
+        a.href = dataUrl;
+        a.setAttribute("download", "chart");
+        a.setAttribute("id", "asdasd");
+        a.click();
       }}
       className={clsx(
         "px-2 py-1 rounded font-work text-lg focus:outline-none focus:ring",
@@ -26,6 +29,6 @@ export default function DownloadButton() {
       )}
     >
       Download
-    </a>
+    </button>
   );
 }
