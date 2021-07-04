222f3e70a84f4af4ecb3df3201a1e608a1057901
Dan Wood <daniel4wood@icloud.com>
2021-07-05 01:55:17 +1000

    Change base to / for migration off GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import eslintPlugin from "@nabla/vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), svgr(), eslintPlugin()],
-  base: "/music-list/",
+  base: "/",
   server: {
     host: "0.0.0.0",
   },
